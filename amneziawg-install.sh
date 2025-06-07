df6b2c17494e2b0a73ac24b586dab108d185ae98
samara1531 <samara1531@gmail.com>
2025-06-07 20:00:52 +0300

    Update amneziawg-install.sh

--- amneziawg-install.sh
+++ amneziawg-install.sh
@@ -216,15 +216,3 @@ if [ "$RESTART_NETWORK" = "y" ] || [ "$RESTART_NETWORK" = "Y" ]; then
 else
     echo -e "${YELLOW}Вы можете вручную перезапустить сеть командой: ${GREEN}service network stop && service network start${RESET}"
 fi
-
-echo -e "${YELLOW}Требуется перезапустить сетевые службы, сделать это сейчас? (y/n): ${RESET}"
-read RESTART_NETWORK
-
-if [ "$RESTART_NETWORK" = "y" ] || [ "$RESTART_NETWORK" = "Y" ]; then
-    echo -e "Перезапуск сети..."
-    service network stop
-    sleep 2
-    service network start
-else
-    echo -e "${YELLOW}Вы можете вручную перезапустить сеть командой: ${GREEN}service network stop && service network start${RESET}"
-fi
